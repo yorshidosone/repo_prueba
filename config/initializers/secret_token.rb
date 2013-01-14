3c44018fc018c1d9b31ab853d21b8c8ed3db0929
Jorge <jacevedo@aggerosolutions.com>
2013-01-14 13:45:18 -0700

    CRUDs

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Facturas::Application.config.secret_token = 'd7ed2ac881f4f2901e48ea646c7667e9b34528248e287a68cdba5610c76742c3c05543f2996d556e34fa1b097cc27e12914b31b2956aa3dfb28f6689dcf71466'
